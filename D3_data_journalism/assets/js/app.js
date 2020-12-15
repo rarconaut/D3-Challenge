b91cca935962fa2e969b197cc377303b8e8c9386
rarconaut <68246130+rarconaut@users.noreply.github.com>
2020-12-14 19:31:18 -0800

    Trying to add state abbr to center of circles.

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -5,10 +5,10 @@ var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
 };
 
 var chartWidth = svgWidth - margin.left - margin.right;
@@ -17,34 +17,34 @@ var chartHeight = svgHeight - margin.top - margin.bottom;
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by 
 // left and top margins.
 var svg = d3.select("#scatter")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("assets/data/data.csv").then(function(healthData) {
+d3.csv("assets/data/data.csv").then(function (healthData) {
 
     // Step 1: Parse Data/Cast as numbers
     // ==============================
-    healthData.forEach(function(data) {
-        console.log(data.state, data.income);
-    //   data.hair_length = +data.income;
-    //   data.num_hits = +data.income;
-    //   console.log(data.state, data.income)
+    healthData.forEach(function (data) {
+        // console.log(data.state, data.income);
+        //   data.hair_length = +data.income;
+        //   data.num_hits = +data.income;
+        //   console.log(data.state, data.income)
     });
 
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([d3.min(healthData, d => d.income), d3.max(healthData, d => d.income)])
-      .range([0, chartWidth]);
+        .domain([d3.min(healthData, d => d.income), d3.max(healthData, d => d.income)])
+        .range([0, chartWidth]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([d3.min(healthData, d => d.obesity), d3.max(healthData, d => d.obesity)])
-      .range([chartHeight, 0]);
+        .domain([d3.min(healthData, d => d.obesity), d3.max(healthData, d => d.obesity)])
+        .range([chartHeight, 0]);
 
     // Step 3: Create axis functions
     // ==============================
@@ -54,63 +54,72 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     // Step 4: Append Axes to the chart
     // ==============================
     chartGroup.append("g")
-      .attr("transform", `translate(0, ${chartHeight})`)
-      .call(bottomAxis);
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
 
     chartGroup.append("g")
-      .call(leftAxis);
+        .call(leftAxis);
 
     // Step 5: Create Circles
     // ==============================
     var circlesGroup = chartGroup.selectAll("circle")
-    .data(healthData)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d.income))
-    .attr("cy", d => yLinearScale(d.obesity))
-    .attr("r", "15")
-    .attr("fill", "blue")
-    .attr("opacity", ".5")
-    .text(`${d.abbr}`);
+        .data(healthData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.income))
+        .attr("cy", d => yLinearScale(d.obesity))
+        .attr("r", "15")
+        .attr("fill", "blue")
+        .attr("opacity", ".5");
 
-    // chartGroup.call(circlesGroup);
+    /* Create the text for each block */
+    circlesGroup.append("text")
+        .text(function (d) {
+            return d.abbr
+        });
+        // .attr({
+        //     "text-anchor": "middle",
+        //     // "font-size": function (d) {
+        //     //     return d.r / ((d.r * 10) / 100);
+        //     // }
+        // });
 
-//     // Step 6: Initialize tool tip
-//     // ==============================
-//     var toolTip = d3.tip()
-//       .classed("tooltip")
-//       .offset([80, -60])
-//       .html(function(d) {
-//         return (`${d.rockband}<br>Hair length: ${d.hair_length}<br>Hits: ${d.num_hits}`);
-//       });
+    //     // Step 6: Initialize tool tip
+    //     // ==============================
+    //     var toolTip = d3.tip()
+    //       .classed("tooltip", true)
+    //       .offset([80, -60])
+    //       .html(function(d) {
+    //         return (`${d.rockband}<br>Hair length: ${d.hair_length}<br>Hits: ${d.num_hits}`);
+    //       });
 
-//     // Step 7: Create tooltip in the chart
-//     // ==============================
-//     chartGroup.call(toolTip);
+    //     // Step 7: Create tooltip in the chart
+    //     // ==============================
+    //     chartGroup.call(toolTip);
 
-//     // Step 8: Create event listeners to display and hide the tooltip
-//     // ==============================
-//     circlesGroup.on("click", function(data) {
-//       toolTip.show(data, this);
-//     })
-//       // onmouseout event
-//       .on("mouseout", function(data, index) {
-//         toolTip.hide(data);
-//       });
+    //     // Step 8: Create event listeners to display and hide the tooltip
+    //     // ==============================
+    //     circlesGroup.on("click", function(data) {
+    //       toolTip.show(data, this);
+    //     })
+    //       // onmouseout event
+    //       .on("mouseout", function(data, index) {
+    //         toolTip.hide(data);
+    //       });
 
-//     // Create axes labels
-//     chartGroup.append("text")
-//       .attr("transform", "rotate(-90)")
-//       .attr("y", 0 - margin.left + 40)
-//       .attr("x", 0 - (height / 2))
-//       .attr("dy", "1em")
-//       .attr("class", "axisText")
-//       .text("Number of Billboard 100 Hits");
+    //     // Create axes labels
+    //     chartGroup.append("text")
+    //       .attr("transform", "rotate(-90)")
+    //       .attr("y", 0 - margin.left + 40)
+    //       .attr("x", 0 - (height / 2))
+    //       .attr("dy", "1em")
+    //       .attr("class", "axisText")
+    //       .text("Number of Billboard 100 Hits");
 
-//     chartGroup.append("text")
-//       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-//       .attr("class", "axisText")
-//       .text("Hair Metal Band Hair Length (inches)");
-  }).catch(function(error) {
+    //     chartGroup.append("text")
+    //       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    //       .attr("class", "axisText")
+    //       .text("Hair Metal Band Hair Length (inches)");
+}).catch(function (error) {
     console.log(error);
-  });
+});
