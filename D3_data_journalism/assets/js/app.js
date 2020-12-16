9e63be4716e60e843c4a13deac8602540e0b692f
rarconaut <68246130+rarconaut@users.noreply.github.com>
2020-12-15 23:46:12 -0800

    Changed the margins and label locations to minimize cutoff of peripheral datapoints tooltip transitions. Everything seems to be functioning normally. Core Assignment-D3 Dabbler complete.

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,11 +1,12 @@
 // @TODO: YOUR CODE HERE!
-
+// Homework 16-D3 Challenge
 // Outline for homework adapted from 16/D3/3/Activities/09-Stu_Hair_Metal
+
 var svgWidth = 960;
-var svgHeight = 500;
+var svgHeight = 540;
 
 var margin = {
-    top: 20,
+    top: 60,
     right: 40,
     bottom: 60,
     left: 100
@@ -31,9 +32,6 @@ d3.csv("assets/data/data.csv").then(function (healthData) {
     // ==============================
     healthData.forEach(function (data) {
         // console.log(data.state, data.income);
-        //   data.hair_length = +data.income;
-        //   data.num_hits = +data.income;
-        //   console.log(data.state, data.income)
     });
 
     // Step 2: Create scale functions
@@ -62,6 +60,15 @@ d3.csv("assets/data/data.csv").then(function (healthData) {
         .classed("y", true)
         .call(leftAxis);
 
+    /* Create the text for each circle */
+    var textGroup = chartGroup.selectAll("g.dot")
+        .data(healthData)
+        .enter()
+        .append("text")
+        .attr("x", d => xLinearScale(d.income) - 10)
+        .attr("y", d => yLinearScale(d.obesity) + 5)
+        .text((d, i) => d.abbr)
+
     // Step 5: Create Circles
     // ==============================
     var circlesGroup = chartGroup.append("g")
@@ -76,16 +83,7 @@ d3.csv("assets/data/data.csv").then(function (healthData) {
         .attr("opacity", ".5")
         .classed("dot", true);
 
-    // /* Create the text for each circle */
-    var textGroup = chartGroup.selectAll("g.dot")
-        .data(healthData)
-        .enter()
-        .append("text")
-        .text((d, i) => d.abbr[i])
-        .attr({
-            'font-size': 8, //font size
-            "text-anchor": "middle" //positions text in center of the circle
-        });
+
 
     // Event handlers for interactive scatter points 
     circlesGroup.on("click", function (healthData) {
@@ -111,7 +109,7 @@ d3.csv("assets/data/data.csv").then(function (healthData) {
                     .duration(500)
                     .attr("r", "15");
             }
-            // for unclicked points, returns to original size and color
+            // for unclicked points, remains their original size and color
             else {
                 // console.log(this);
                 d3.select(this)
@@ -123,47 +121,45 @@ d3.csv("assets/data/data.csv").then(function (healthData) {
             }
         });
 
-        // Step 6: Initialize tool tip
-        // ==============================
+    // Step 6: Initialize tool tip
+    // ==============================
     var toolTip = d3.tip()
         .attr("class", "tooltip")
         .offset([10, 0])
-        // .attr({
-        //     // 'font-size': 8, //font size
-        //     "text-anchor": "middle" //positions text in center of the circle
-        // })
         .html(function (d) {
             return (`${d.state}<br>Income: ${d.income}<br>Obesity: ${d.obesity}`);
         });
 
-        // Step 7: Create tooltip in the chart
-        // ==============================
-        chartGroup.call(toolTip);
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
 
-        // Step 8: Create event listeners to display and hide the tooltip
-        // ==============================
-        // circlesGroup.on("click", function(healthData) {
-        //   toolTip.show(healthData, this);
-        // })
-        //   // onmouseout event
-        //   .on("mouseout", function(healthData, index) {
-            // toolTip.hide(healthData);
-        //   });
+    // Step 8: Create event listeners to display and hide the tooltip  
+    // Instead ===> I incorporated this step into my own tooltip event (lines 88-122 :) 
+    // ==============================
+    // circlesGroup.on("click", function(healthData) {
+    //   toolTip.show(healthData, this);
+    // })
+    //   // onmouseout event
+    //   .on("mouseout", function(healthData, index) {
+    // toolTip.hide(healthData);
+    //   });
 
-        // Step 9: Create axes labels
-        // ==============================
-        chartGroup.append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 0 - margin.left + 40)
-          .attr("x", 0 - (chartHeight / 2))
-          .attr("dy", "1em")
-          .attr("class", "axisText")
-          .text("Obesity Score");
+    // Step 9: Create axes labels
+    // ==============================
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Obesity Score");
 
-        chartGroup.append("text")
-          .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top + 30})`)
-          .attr("class", "axisText")
-          .text("Income ($USD)");
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top - 20})`)
+        .attr("class", "axisText")
+        .text("Income ($USD)");
+
 }).catch(function (error) {
     console.log(error);
 });
