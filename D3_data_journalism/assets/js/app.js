1bf50809e48856a3fc77e52dcc17a482b053b70a
rarconaut <68246130+rarconaut@users.noreply.github.com>
2020-12-15 23:09:53 -0800

    Added axis labels, and made the tool tip/event transitions less buggy.

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -93,7 +93,7 @@ d3.csv("assets/data/data.csv").then(function (healthData) {
         d3.select(this)
             .transition()
             .duration(500)
-            .attr("r", "75")
+            .attr("r", "85")
             .attr("fill", "red")
             .attr("opacity", ".99")
             .attr("class", "clicked");
@@ -127,7 +127,11 @@ d3.csv("assets/data/data.csv").then(function (healthData) {
         // ==============================
     var toolTip = d3.tip()
         .attr("class", "tooltip")
-        .offset([15, 0])
+        .offset([10, 0])
+        // .attr({
+        //     // 'font-size': 8, //font size
+        //     "text-anchor": "middle" //positions text in center of the circle
+        // })
         .html(function (d) {
             return (`${d.state}<br>Income: ${d.income}<br>Obesity: ${d.obesity}`);
         });
@@ -146,19 +150,20 @@ d3.csv("assets/data/data.csv").then(function (healthData) {
             // toolTip.hide(healthData);
         //   });
 
-    //     // Create axes labels
-    //     chartGroup.append("text")
-    //       .attr("transform", "rotate(-90)")
-    //       .attr("y", 0 - margin.left + 40)
-    //       .attr("x", 0 - (height / 2))
-    //       .attr("dy", "1em")
-    //       .attr("class", "axisText")
-    //       .text("Number of Billboard 100 Hits");
+        // Step 9: Create axes labels
+        // ==============================
+        chartGroup.append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 0 - margin.left + 40)
+          .attr("x", 0 - (chartHeight / 2))
+          .attr("dy", "1em")
+          .attr("class", "axisText")
+          .text("Obesity Score");
 
-    //     chartGroup.append("text")
-    //       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-    //       .attr("class", "axisText")
-    //       .text("Hair Metal Band Hair Length (inches)");
+        chartGroup.append("text")
+          .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top + 30})`)
+          .attr("class", "axisText")
+          .text("Income ($USD)");
 }).catch(function (error) {
     console.log(error);
 });
